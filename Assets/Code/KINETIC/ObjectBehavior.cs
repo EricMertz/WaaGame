b33bdeef4c4e02dbc1f2af0754f4e5ab2383f0f8
Mertzstache <ericm338@Erics-MacBook-Pro-2.local>
2017-03-02 01:36:09 -0600

    kinetic

---
+++ Assets/Code/KINETIC/ObjectBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehavior : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+            Debug.Log("down");
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        rb.velocity = new Vector3(rb.velocity.x, Mathf.Abs(rb.velocity.y));
+    }
+
+}
---
+++ Assets/Code/KINETIC/ObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+    public GameObject obj; //prefab for freeze enemies
+
+    public float SpawnRate;
+
+    private float SpawnTimer;
+
+    // Use this for initialization
+    void Start () {
+        GameObject temp = Instantiate(obj, transform.position, Quaternion.identity);
+        temp.GetComponent<Rigidbody2D>().AddForce(temp.transform.right * Random.Range(-50, 50));
+        SpawnTimer = Time.time + SpawnRate;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (SpawnTimer < Time.time)
+        {
+            GameObject temp = Instantiate(obj, transform.position, Quaternion.identity);
+            temp.GetComponent<Rigidbody2D>().AddForce(temp.transform.right * Random.Range(-50, 50));
+            SpawnTimer = Time.time + SpawnRate;
+        }
+    }
+}
